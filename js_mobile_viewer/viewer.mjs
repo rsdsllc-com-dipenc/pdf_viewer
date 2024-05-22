4cdfdbf5f57f86b0b270c0fc96f2e4aec16c3078
Dipen Chauhan <dipenc@rsdsllc.com>
2024-05-22 11:36:59 -0500

    made simpler

--- js_mobile_viewer/viewer.mjs
+++ js_mobile_viewer/viewer.mjs
@@ -1,356 +1,181 @@
 if (!pdfjsLib.getDocument || !pdfjsViewer.PDFViewer) {
-  // eslint-disable-next-line no-alert
-  alert("Please build the pdfjs-dist library using\n `gulp dist-install`");
+  alert("Please build the pdfjs-dist library using `gulp dist-install`");
 }
 
-const MAX_CANVAS_PIXELS = 0; // CSS-only zooming.
-const TEXT_LAYER_MODE = 2; // ENABLE
+const MAX_CANVAS_PIXELS = 0;
+const TEXT_LAYER_MODE = 2;
 const MAX_IMAGE_SIZE = 1024 * 1024;
 const CMAP_URL = "../../node_modules/pdfjs-dist/cmaps/";
 const CMAP_PACKED = true;
-
-pdfjsLib.GlobalWorkerOptions.workerSrc =
-  "../../node_modules/pdfjs-dist/build/pdf.worker.mjs";
-
 const DEFAULT_URL = "insurance.pdf";
 const DEFAULT_SCALE_DELTA = 1.1;
 const MIN_SCALE = 0.25;
 const MAX_SCALE = 10.0;
 const DEFAULT_SCALE_VALUE = "auto";
 
-const PDFViewerApplication = {
-  pdfLoadingTask: null,
-  pdfDocument: null,
-  pdfViewer: null,
-  pdfHistory: null,
-  pdfLinkService: null,
-  eventBus: null,
+pdfjsLib.GlobalWorkerOptions.workerSrc = "../../node_modules/pdfjs-dist/build/pdf.worker.mjs";
 
-  /**
-   * Opens PDF document specified by URL.
-   * @returns {Promise} - Returns the promise, which is resolved when document
-   *                      is opened.
-   */
-  open(params) {
-    if (this.pdfLoadingTask) {
-      // We need to destroy already opened document
-      return this.close().then(
-        function () {
-          // ... and repeat the open() call.
-          return this.open(params);
-        }.bind(this)
-      );
-    }
+let pdfDocument = null;
+let pdfViewer = null;
+let pdfLinkService = null;
+let pdfHistory = null;
 
-    const url = params.url;
-    const self = this;
-    this.setTitleUsingUrl(url);
+async function openPDF(params) {
+  if (pdfDocument) {
+    await closePDF();
+    return openPDF(params);
+  }
 
-    // Loading document.
-    const loadingTask = pdfjsLib.getDocument({
-      url,
-      maxImageSize: MAX_IMAGE_SIZE,
-      cMapUrl: CMAP_URL,
-      cMapPacked: CMAP_PACKED,
-    });
-    this.pdfLoadingTask = loadingTask;
+  const url = params.url;
+  setTitleUsingUrl(url);
 
-    loadingTask.onProgress = function (progressData) {
-      self.progress(progressData.loaded / progressData.total);
-    };
-
-    return loadingTask.promise.then(
-      function (pdfDocument) {
-        // Document loaded, specifying document for the viewer.
-        self.pdfDocument = pdfDocument;
-        self.pdfViewer.setDocument(pdfDocument);
-        self.pdfLinkService.setDocument(pdfDocument);
-        self.pdfHistory.initialize({
-          fingerprint: pdfDocument.fingerprints[0],
-        });
-
-        self.loadingBar.hide();
-        self.setTitleUsingMetadata(pdfDocument);
-      },
-      function (reason) {
-        let key = "pdfjs-loading-error";
-        if (reason instanceof pdfjsLib.InvalidPDFException) {
-          key = "pdfjs-invalid-file-error";
-        } else if (reason instanceof pdfjsLib.MissingPDFException) {
-          key = "pdfjs-missing-file-error";
-        } else if (reason instanceof pdfjsLib.UnexpectedResponseException) {
-          key = "pdfjs-unexpected-response-error";
-        }
-        self.l10n.get(key).then(msg => {
-          self.error(msg, { message: reason?.message });
-        });
-        self.loadingBar.hide();
-      }
-    );
-  },
-
-  /**
-   * Closes opened PDF document.
-   * @returns {Promise} - Returns the promise, which is resolved when all
-   *                      destruction is completed.
-   */
-  close() {
-    if (!this.pdfLoadingTask) {
-      return Promise.resolve();
-    }
-
-    const promise = this.pdfLoadingTask.destroy();
-    this.pdfLoadingTask = null;
-
-    if (this.pdfDocument) {
-      this.pdfDocument = null;
-
-      this.pdfViewer.setDocument(null);
-      this.pdfLinkService.setDocument(null, null);
-
-      if (this.pdfHistory) {
-        this.pdfHistory.reset();
-      }
-    }
-
-    return promise;
-  },
-
-  get loadingBar() {
-    const bar = document.getElementById("loadingBar");
-    return pdfjsLib.shadow(
-      this,
-      "loadingBar",
-      new pdfjsViewer.ProgressBar(bar)
-    );
-  },
-
-  setTitleUsingUrl: function pdfViewSetTitleUsingUrl(url) {
-    this.url = url;
-    let title = pdfjsLib.getFilenameFromUrl(url) || url;
-    try {
-      title = decodeURIComponent(title);
-    } catch {
-      // decodeURIComponent may throw URIError,
-      // fall back to using the unprocessed url in that case
-    }
-    this.setTitle(title);
-  },
-
-  setTitleUsingMetadata(pdfDocument) {
-    const self = this;
-    pdfDocument.getMetadata().then(function (data) {
-      const info = data.info,
-        metadata = data.metadata;
-      self.documentInfo = info;
-      self.metadata = metadata;
-
-      // Provides some basic debug information
-      console.log(
-        "PDF " +
-          pdfDocument.fingerprints[0] +
-          " [" +
-          info.PDFFormatVersion +
-          " " +
-          (info.Producer || "-").trim() +
-          " / " +
-          (info.Creator || "-").trim() +
-          "]" +
-          " (PDF.js: " +
-          (pdfjsLib.version || "-") +
-          ")"
-      );
-
-      let pdfTitle;
-      if (metadata && metadata.has("dc:title")) {
-        const title = metadata.get("dc:title");
-        // Ghostscript sometimes returns 'Untitled', so prevent setting the
-        // title to 'Untitled.
-        if (title !== "Untitled") {
-          pdfTitle = title;
-        }
-      }
-
-      if (!pdfTitle && info && info.Title) {
-        pdfTitle = info.Title;
-      }
-
-      if (pdfTitle) {
-        self.setTitle(pdfTitle + " - " + document.title);
-      }
-    });
-  },
-
-  setTitle: function pdfViewSetTitle(title) {
-    document.title = title;
-    document.getElementById("title").textContent = title;
-  },
-
-  error: function pdfViewError(message, moreInfo) {
-    const moreInfoText = [
-      `PDF.js v${pdfjsLib.version || "?"} (build: ${pdfjsLib.build || "?"})`,
-    ];
-    if (moreInfo) {
-      moreInfoText.push(`Message: ${moreInfo.message}`);
-
-      if (moreInfo.stack) {
-        moreInfoText.push(`Stack: ${moreInfo.stack}`);
-      } else {
-        if (moreInfo.filename) {
-          moreInfoText.push(`File: ${moreInfo.filename}`);
-        }
-        if (moreInfo.lineNumber) {
-          moreInfoText.push(`Line: ${moreInfo.lineNumber}`);
-        }
-      }
-    }
-
-    console.error(`${message}\n\n${moreInfoText.join("\n")}`);
-  },
-
-  progress: function pdfViewProgress(level) {
-    const percent = Math.round(level * 100);
-    // Updating the bar if value increases.
-    if (percent > this.loadingBar.percent || isNaN(percent)) {
-      this.loadingBar.percent = percent;
-    }
-  },
-
-  get pagesCount() {
-    return this.pdfDocument.numPages;
-  },
-
-  get page() {
-    return this.pdfViewer.currentPageNumber;
-  },
-
-  set page(val) {
-    this.pdfViewer.currentPageNumber = val;
-  },
-
-  zoomIn: function pdfViewZoomIn(ticks) {
-    let newScale = this.pdfViewer.currentScale;
-    do {
-      newScale = (newScale * DEFAULT_SCALE_DELTA).toFixed(2);
-      newScale = Math.ceil(newScale * 10) / 10;
-      newScale = Math.min(MAX_SCALE, newScale);
-    } while (--ticks && newScale < MAX_SCALE);
-    this.pdfViewer.currentScaleValue = newScale;
-  },
-
-  zoomOut: function pdfViewZoomOut(ticks) {
-    let newScale = this.pdfViewer.currentScale;
-    do {
-      newScale = (newScale / DEFAULT_SCALE_DELTA).toFixed(2);
-      newScale = Math.floor(newScale * 10) / 10;
-      newScale = Math.max(MIN_SCALE, newScale);
-    } while (--ticks && newScale > MIN_SCALE);
-    this.pdfViewer.currentScaleValue = newScale;
-  },
-
-  initUI: function pdfViewInitUI() {
-    const eventBus = new pdfjsViewer.EventBus();
-    this.eventBus = eventBus;
-
-    const linkService = new pdfjsViewer.PDFLinkService({
-      eventBus,
-    });
-    this.pdfLinkService = linkService;
-
-    this.l10n = new pdfjsViewer.GenericL10n();
-
-    const container = document.getElementById("viewerContainer");
-    const pdfViewer = new pdfjsViewer.PDFViewer({
-      container,
-      eventBus,
-      linkService,
-      l10n: this.l10n,
-      maxCanvasPixels: MAX_CANVAS_PIXELS,
-      textLayerMode: TEXT_LAYER_MODE,
-    });
-    this.pdfViewer = pdfViewer;
-    linkService.setViewer(pdfViewer);
-
-    this.pdfHistory = new pdfjsViewer.PDFHistory({
-      eventBus,
-      linkService,
-    });
-    linkService.setHistory(this.pdfHistory);
-
-    document.getElementById("previous").addEventListener("click", function () {
-      PDFViewerApplication.page--;
-    });
-
-    document.getElementById("next").addEventListener("click", function () {
-      PDFViewerApplication.page++;
-    });
-
-    document.getElementById("zoomIn").addEventListener("click", function () {
-      PDFViewerApplication.zoomIn();
-    });
-
-    document.getElementById("zoomOut").addEventListener("click", function () {
-      PDFViewerApplication.zoomOut();
-    });
-
-    document
-      .getElementById("pageNumber")
-      .addEventListener("click", function () {
-        this.select();
-      });
-
-    document
-      .getElementById("pageNumber")
-      .addEventListener("change", function () {
-        PDFViewerApplication.page = this.value | 0;
-
-        // Ensure that the page number input displays the correct value,
-        // even if the value entered by the user was invalid
-        // (e.g. a floating point number).
-        if (this.value !== PDFViewerApplication.page.toString()) {
-          this.value = PDFViewerApplication.page;
-        }
-      });
-
-    eventBus.on("pagesinit", function () {
-      // We can use pdfViewer now, e.g. let's change default scale.
-      pdfViewer.currentScaleValue = DEFAULT_SCALE_VALUE;
-    });
-
-    eventBus.on(
-      "pagechanging",
-      function (evt) {
-        const page = evt.pageNumber;
-        const numPages = PDFViewerApplication.pagesCount;
-
-        document.getElementById("pageNumber").value = page;
-        document.getElementById("previous").disabled = page <= 1;
-        document.getElementById("next").disabled = page >= numPages;
-      },
-      true
-    );
-  },
-};
-
-window.PDFViewerApplication = PDFViewerApplication;
-
-document.addEventListener(
-  "DOMContentLoaded",
-  function () {
-    PDFViewerApplication.initUI();
-  },
-  true
-);
-
-// The offsetParent is not set until the PDF.js iframe or object is visible;
-// waiting for first animation.
-const animationStarted = new Promise(function (resolve) {
-  window.requestAnimationFrame(resolve);
-});
-
-// We need to delay opening until all HTML is loaded.
-animationStarted.then(function () {
-  PDFViewerApplication.open({
-    url: DEFAULT_URL,
+  const loadingTask = pdfjsLib.getDocument({
+    url,
+    maxImageSize: MAX_IMAGE_SIZE,
+    cMapUrl: CMAP_URL,
+    cMapPacked: CMAP_PACKED,
   });
+
+  loadingTask.onProgress = (progressData) => {
+    updateProgress(progressData.loaded / progressData.total);
+  };
+
+  try {
+    pdfDocument = await loadingTask.promise;
+    pdfViewer.setDocument(pdfDocument);
+    pdfLinkService.setDocument(pdfDocument);
+    pdfHistory.initialize({ fingerprint: pdfDocument.fingerprints[0] });
+    hideLoadingBar();
+    setTitleUsingMetadata(pdfDocument);
+  } catch (reason) {
+    handleError(reason);
+  }
+}
+
+function closePDF() {
+  if (!pdfDocument) return Promise.resolve();
+
+  const promise = pdfDocument.destroy();
+  pdfDocument = null;
+
+  pdfViewer.setDocument(null);
+  pdfLinkService.setDocument(null, null);
+
+  if (pdfHistory) pdfHistory.reset();
+
+  return promise;
+}
+
+function setTitleUsingUrl(url) {
+  const title = pdfjsLib.getFilenameFromUrl(url) || url;
+  document.title = title;
+  document.getElementById("title").textContent = title;
+}
+
+function setTitleUsingMetadata(pdf) {
+  pdf.getMetadata().then(({ info, metadata }) => {
+    const title = metadata?.has("dc:title") ? metadata.get("dc:title") : info?.Title;
+    if (title && title !== "Untitled") {
+      document.title = `${title} - ${document.title}`;
+      document.getElementById("title").textContent = document.title;
+    }
+  });
+}
+
+function handleError(reason) {
+  const errorMsg = reason instanceof pdfjsLib.InvalidPDFException
+    ? "Invalid PDF"
+    : reason instanceof pdfjsLib.MissingPDFException
+      ? "Missing PDF"
+      : "Unexpected Response";
+  console.error(`${errorMsg}: ${reason.message}`);
+  hideLoadingBar();
+}
+
+function updateProgress(level) {
+  const percent = Math.round(level * 100);
+  const loadingBar = document.getElementById("loadingBar");
+  loadingBar.style.width = `${percent}%`;
+}
+
+function hideLoadingBar() {
+  document.getElementById("loadingBar").style.display = "none";
+}
+
+function zoomIn() {
+  let newScale = pdfViewer.currentScale;
+  newScale = Math.min(MAX_SCALE, newScale * DEFAULT_SCALE_DELTA);
+  pdfViewer.currentScaleValue = newScale;
+}
+
+function zoomOut() {
+  let newScale = pdfViewer.currentScale;
+  newScale = Math.max(MIN_SCALE, newScale / DEFAULT_SCALE_DELTA);
+  pdfViewer.currentScaleValue = newScale;
+}
+
+function initUI() {
+  const eventBus = new pdfjsViewer.EventBus();
+  pdfLinkService = new pdfjsViewer.PDFLinkService({ eventBus });
+  pdfHistory = new pdfjsViewer.PDFHistory({ eventBus, linkService: pdfLinkService });
+
+  const container = document.getElementById("viewerContainer");
+  pdfViewer = new pdfjsViewer.PDFViewer({
+    container,
+    eventBus,
+    linkService: pdfLinkService,
+    l10n: new pdfjsViewer.GenericL10n(),
+    maxCanvasPixels: MAX_CANVAS_PIXELS,
+    textLayerMode: TEXT_LAYER_MODE,
+  });
+
+  pdfLinkService.setViewer(pdfViewer);
+  pdfLinkService.setHistory(pdfHistory);
+
+  document.getElementById("previous").addEventListener("click", () => pdfViewer.currentPageNumber--);
+  document.getElementById("next").addEventListener("click", () => pdfViewer.currentPageNumber++);
+  document.getElementById("zoomIn").addEventListener("click", zoomIn);
+  document.getElementById("zoomOut").addEventListener("click", zoomOut);
+
+  document.getElementById("pageNumber").addEventListener("change", function () {
+    pdfViewer.currentPageNumber = this.value | 0;
+  });
+
+  eventBus.on("pagesinit", () => {
+    pdfViewer.currentScaleValue = DEFAULT_SCALE_VALUE;
+  });
+
+  eventBus.on("pagechanging", (evt) => {
+    const page = evt.pageNumber;
+    document.getElementById("pageNumber").value = page;
+    document.getElementById("previous").disabled = page <= 1;
+    document.getElementById("next").disabled = page >= pdfViewer.pagesCount;
+  });
+}
+
+async function highlightWords(pageIndex, words) {
+  const page = await pdfDocument.getPage(pageIndex);
+  const textContent = await page.getTextContent();
+  const viewport = page.getViewport({ scale: pdfViewer.currentScale });
+  const textLayerDiv = pdfViewer.getPageView(pageIndex - 1).textLayer.textLayerDiv;
+
+  textContent.items.forEach((item) => {
+    if (words.includes(item.str)) {
+      const highlightDiv = document.createElement("div");
+      highlightDiv.className = "highlight";
+      highlightDiv.style.left = `${item.transform[4]}px`;
+      highlightDiv.style.top = `${viewport.height - item.transform[5]}px`;
+      highlightDiv.style.width = `${item.width * viewport.scale}px`;
+      highlightDiv.style.height = `${item.height * viewport.scale}px`;
+      highlightDiv.style.backgroundColor = "yellow";
+      textLayerDiv.appendChild(highlightDiv);
+    }
+  });
+}
+
+document.addEventListener("DOMContentLoaded", async () => {
+  await initUI();
+  await openPDF({ url: DEFAULT_URL });
+  await highlightWords(1, ["law"]);
+  // await highlightWords(2, ["important"]);
 });
